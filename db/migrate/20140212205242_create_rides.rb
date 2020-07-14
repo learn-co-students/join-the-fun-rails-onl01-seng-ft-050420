57804d43e2db561551daf17a2c8913ffc1e0d0da
leobr <leobrionesherrera@gmail.com>
2020-07-14 13:42:55 -0500

    Done.

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -2,6 +2,8 @@ class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
       t.timestamps null: false
+      t.references :taxi
+      t.references :passenger
     end
   end
 end
